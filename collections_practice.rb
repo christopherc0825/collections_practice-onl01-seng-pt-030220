2d49a6a7b6aafaeb46240ad70c933ab74dc0ff63
Christopher Chaison <cchaison0@gmail.com>
2020-03-05 00:37:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,3 +40,11 @@ end
 def sum_array(array)
   array.inject {|sum, n| sum + n}
 end
+
+def add_s(array)
+  array.each_with_index.collect do |el, index|
+    if index != 1
+      el << "s"
+    end
+  end
+end
